a9211fc5ff7b4f04fbc9db39e15fd3d0608d5c55
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2021-03-01 23:46:32 +0530

    Create B.cpp

---
+++ B.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n; 
+    cin >> n ; 
+    string s; 
+    cin >> s; 
+    cout << (count(s.begin(), s.end(), '0')&1 ? "NO" : "YES") ; 
+}
