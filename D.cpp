a39669c650307c92e55037f305f04b2a5a100786
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2021-03-01 23:47:44 +0530

    Create D.cpp

---
+++ D.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+    int n, m; cin >> n >> m; 
+    if(n-1 > m or (n == 3 and m == 2)) {
+        return cout <<"NO", 0 ; 
+    }
+ 
+    int a[n][m] = {} ;
+    for(int i = 0 ; i < m; ++i) a[0][i] = i ; 
+ 
+    for(int i = 1 ; i < n ; ++i) {
+        for(int j = i-1 ; j < m ; ++j) a[i][j] = j ; 
+        for(int j = 0 ; j < i-2 ; ++j) a[i][j] = j+1;
+        if(i-2 >= 0) a[i][i-2] = 0 ;  
+    }
+ 
+    a[2][0] = 2, a[2][2] = 0 ; 
+ 
+    cout << "YES\n" ; 
+    for(int i = 0 ; i < n ; ++i) {
+        for(int j = 0 ; j < m ; ++j) 
+            cout << a[i][j]+1 << " " ; 
+        cout << '\n' ;
+    }
+ 
+    return 0 ; 
+}
