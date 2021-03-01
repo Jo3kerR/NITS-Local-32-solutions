caf00aa4e64649c79eb865a46a4f05e4ccf120c9
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2021-03-01 23:57:49 +0530

    Update E.cpp

--- E.cpp
+++ E.cpp
@@ -1,34 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
- 
-int main() {
- 
-    ios::sync_with_stdio(0); 
-    cin.tie(0); 
- 
-    int tt; cin >> tt ; 
- 
-    while(tt--) {
-        int n, w;
-        cin >> n >> w ;
-        vector<long long> v(n) ; 
-        for(auto &i : v)
-            cin >> i ; 
-        vector<long long> dp(n), mx(n) ;
-        dp[0] = 0 ; 
-        mx[0] = -v[0] ; 
-        for(int i = 1 ; i < n ; ++i) {
-            if(i-w >= 0) {
-                dp[i] = max(dp[i-1], v[i] + mx[i-w]) ;
-                mx[i] = max(mx[i-1], dp[i-w] - v[i]) ;  
+#define ll long long
+
+int main() 
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int tc; cin>>tc;
+    while(tc--)
+    {
+        int n, w; 
+        cin >> n >> w;
+        ll a[n];
+        for(int i = 0 ; i < n ; i++) cin >> a[i];
+
+        vector<ll> dp(n), mx(n);
+        for(int i = 0; i < n ; i++)
+        {
+            if(i-w < 0) 
+            { 
+                dp[i] = 0; 
+                mx[i] = (i==0) ? -a[i] : max(mx[i-1],-a[i]) ; 
             }
-            else {
-                dp[i] = dp[i-1] ; 
-                mx[i] = max(mx[i-1], -v[i]) ; 
+            else
+            {
+                dp[i] = max(dp[i-1], a[i] + mx[i-w]);
+                mx[i] = max(mx[i-1], dp[i-w] - a[i]);
             }
         }
-        cout << dp[n-1] << '\n' ; 
+
+        cout << dp[n-1] << '\n';
     }
- 
-    return 0 ; 
 }
